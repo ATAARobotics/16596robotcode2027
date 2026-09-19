c79b37bf348efe7bcc981c9f2785cc022953d1bc
George Nazarey <gnazarey@naz.com>
2026-09-18 22:33:57 -0600

    Added RGB light constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.Util;
 
 public class Constants {
+    // This is the rgb_light component of the class
+    public static final class RGB_Light {
+        public static final double OFF = 0.0;
+        public static final double BLACK = 0.0;
+        public static final double RED = 0.279;
+        public static final double ORANGE = 0.333;
+        public static final double YELLOW = 0.388;
+        public static final double SAGE = 0.444;
+        public static final double GREEN = 0.5;
+        public static final double AZURE = 0.555;
+        public static final double BLUE =  0.611;
+        public static final double INDIGO = 0.666;
+        public static final double VIOLET = 0.722;
+        public static final double WHITE = 1.000;
+        public static final double ON = 1.000;
+
+        public static final String LOGFILE = "RGBLights.csv";
+        public static final Boolean LOGGING = true;
+    }
 }
